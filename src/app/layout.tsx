de56c4c837760c64d2dd2212a6d8cb96e7650832
Hrithik Dutta <hrithik.dutta@unifize.com>
2025-11-20 22:10:32 +0530

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,11 +92,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
-        <link rel="manifest" href="/manifest.json" />
-      </head>
       <body
         className={`${ibmMono.variable} font-mono antialiased`}
       >
